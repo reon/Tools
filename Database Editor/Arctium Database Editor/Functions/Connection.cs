f8def52a11d37a7cab1aef70d907fd6101f74e61
Fabian <fabian@arctium.org>
2012-10-10 00:03:09 +0200

    Implement showing object list in the datagrid. Note: You have to put the assembly (dll, exe) with the object structs in the application folder.

--- Database Editor/Arctium Database Editor/Functions/Connection.cs
+++ Database Editor/Arctium Database Editor/Functions/Connection.cs
@@ -1,5 +1,10 @@
 ﻿using Db4objects.Db4o;
 using Db4objects.Db4o.CS;
+using Db4objects.Db4o.Internal.Convert;
+using Db4objects.Db4o.Linq;
+using System;
+using System.Collections.Generic;
+using System.Collections;
 
 namespace Arctium_Database_Editor.Functions
 {
@@ -10,6 +15,32 @@ namespace Arctium_Database_Editor.Functions
         public Connection(string host, int port, string user, string password)
         {
             Database = Db4oClientServer.OpenClient(host, port, user, password);
+        }
+
+        public List<T> Select<T>()
+        {
+            var sObject = from T o in Database select o;
+
+            List<T> list = new List<T>();
+            foreach (T obj in sObject)
+                list.Add(obj);
+
+            return list;
+        }
+
+        public Array Select(Type type)
+        {
+            var sObject = from object o in Database select o;
+
+            ArrayList list = new ArrayList();
+            foreach (var obj in sObject)
+                if (obj.GetType() == type)
+                {
+                    list.Add(obj);
+                    break;
+                }
+
+            return list.ToArray(type);
         }
     }
 }
--- Database Editor/Arctium Database Editor/MainWindow.xaml.cs
+++ Database Editor/Arctium Database Editor/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using System.Reflection;
+using System.IO;
 
 namespace Arctium_Database_Editor
 {
@@ -24,8 +27,10 @@ namespace Arctium_Database_Editor
         {
             InitializeComponent();
 
-            if (Globals.Conn == null)
-                DisableButtonsOnDisconnect();
+            foreach (string dll in Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.dll", SearchOption.AllDirectories))
+                Assembly.LoadFile(dll);
+
+            DisableButtonsOnDisconnect();
         }
 
         private void MainExitButton_Click(object sender, RoutedEventArgs e)
@@ -41,9 +46,10 @@ namespace Arctium_Database_Editor
             ObjectAddButton.IsEnabled = true;
             ObjectRemoveButton.IsEnabled = true;
 
-            foreach (var sClass in Globals.Conn.Database.Ext().StoredClasses())
-                if (sClass.InstanceCount() > 0)
-                    ObjectList.Items.Add(sClass.GetName());
+            Globals.Conn.Database.Ext().StoredClasses().ToList().ForEach(sClass =>
+            {
+                ObjectList.Items.Add(sClass.GetName());
+            });
         }
 
         void DisableButtonsOnDisconnect()
@@ -63,6 +69,44 @@ namespace Arctium_Database_Editor
                 Globals.Conn.Database.Close();
 
             DisableButtonsOnDisconnect();
+        }
+
+        private void ObjectList_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            DataTable dt = new DataTable();
+            DataColumn column = null;
+            DataRow row = null;
+
+            var arr = Globals.Conn.Select(Type.GetType(Globals.Conn.Database.Ext().StoredClass(ObjectList.SelectedValue).GetName()));
+
+            Globals.Conn.Database.Ext().StoredClass(ObjectList.SelectedValue).GetStoredFields().ToList().ForEach(field =>
+            {
+                string name = field.GetName().Split(new char[] { '>' })[0].Replace("<", "").Trim();
+
+                column = new DataColumn(name);
+                dt.Columns.Add(column);
+            });
+
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                row = dt.NewRow();
+
+                foreach (DataColumn c in dt.Columns)
+                {
+                    Globals.Conn.Database.Ext().StoredClass(ObjectList.SelectedValue).GetStoredFields().ToList().ForEach(field =>
+                    {
+                        string name = field.GetName().Split(new char[] { '>' })[0].Replace("<", "").Trim();
+
+                        if (c.ColumnName == name)
+                            row[name] = field.Get(arr.GetValue(i));
+                    });
+                }
+
+                dt.Rows.Add(row);
+            }
+
+            ObjectFieldTable.ItemsSource = dt.DefaultView;
         }
     }
 }
