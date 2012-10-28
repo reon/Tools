206f8bad152f0172b74b840e99d8e30b61ac3d68
Fabian <fabian@arctium.org>
2012-10-28 17:19:26 +0100

    Added Arctium Connection Patcher source.

---
+++ Client Patcher/Source/Arctium Connection Patcher/Addresses.cs
@@ -0,0 +1,19 @@
+﻿namespace ArctiumConnectionPatcher
+{
+    enum Offsets : int
+    {
+        // Client movement packets
+        Send2Offset              = 0x3F8F8A,
+        Send2Offset2             = 0x3F8F97,
+        Send2Offset3             = 0x3F8FA1,
+
+        // Client packets
+        LegacyRoutingTableOffset = 0xA075F0,
+
+        // Some server packets
+        CommsHandlerOffset       = 0x3F8948,
+
+        // Allow login with email addresses
+        emailOffset              = 0x78EB4D
+    }
+}
---
+++ Client Patcher/Source/Arctium Connection Patcher/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace ArctiumConnectionPatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+                return;
+
+            string wowBinary = args[0];
+
+            Console.WriteLine("Choose a patch for your WoW binary: \n\n");
+            Console.WriteLine("0: Exit.\n");
+            Console.WriteLine("1: Allow usage of email addresses as username (Login to official servers not possible with this patch).\n");
+            Console.WriteLine("2: Allow packet communication between server and client.\n\n");
+
+            var option = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine(option);
+
+            CreateNewBinary(ref wowBinary);
+
+            switch (option)
+            {
+                case 0:
+                    Environment.Exit(0);
+                    break;
+                case 1:
+                {
+                    byte[] patchedBytes = { 0xEB };
+                    PatchBinary(Offsets.emailOffset, patchedBytes, wowBinary);
+
+                    Console.WriteLine("Exit in 5 seconds...");
+                    Thread.Sleep(5000);
+                    Environment.Exit(0);
+                    break;
+                }
+                case 2:
+                {
+                    Console.WriteLine("Patching send function...");
+
+                    byte[] SendBytes = new byte[2];
+                    PatchBinary(Offsets.Send2Offset, SendBytes, wowBinary);
+                    PatchBinary(Offsets.Send2Offset2, SendBytes, wowBinary);
+                    PatchBinary(Offsets.Send2Offset3, SendBytes, wowBinary);
+
+                    Console.WriteLine("Patching legacy routing table...");
+                    byte[] LegacyRoutingTableBytes = new byte[512];
+                    PatchBinary(Offsets.LegacyRoutingTableOffset, LegacyRoutingTableBytes, wowBinary);
+
+                    Console.WriteLine("Patching receive function...");
+                    byte[] CommsHandlerBytes = { 0xEB };
+                    PatchBinary(Offsets.CommsHandlerOffset, CommsHandlerBytes, wowBinary);
+
+                    Console.WriteLine("Exit in 5 seconds...");
+                    Thread.Sleep(5000);
+                    Environment.Exit(0);
+                    break;
+                }
+                default:
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+
+        static void CreateNewBinary(ref string file)
+        {
+            Console.WriteLine("Creating backup from {0}...", file);
+            File.Copy(file, "WoW.bak", true);
+            Console.WriteLine("Done");
+
+            if (File.Exists("WoW_Patched.exe"))
+                File.Delete("WoW_Patched.exe");
+
+            Console.WriteLine("Create new binary...");
+
+            File.Move(file, "WoW_Patched.exe");
+            File.Move("WoW.bak", file);
+
+            file = "WoW_Patched.exe";
+            Console.WriteLine("Done");
+        }
+
+        static void PatchBinary(Offsets offset, byte[] pBytes, string file)
+        {
+            BinaryReader wowReader = new BinaryReader(File.Open(file, FileMode.Open, FileAccess.Read));
+            wowReader.BaseStream.Seek((int)Offsets.emailOffset, SeekOrigin.Begin);
+
+            if (wowReader.ReadByte() == pBytes[0])
+            {
+                Console.WriteLine("{0} already patched!!!", file);
+                wowReader.Close();
+
+                Console.WriteLine("Exit in 5 seconds...");
+                Thread.Sleep(5000);
+                Environment.Exit(0);
+            }
+            else
+            {
+                wowReader.Close();
+
+                BinaryWriter wowWriter = new BinaryWriter(File.Open(file, FileMode.Open, FileAccess.ReadWrite));
+                wowWriter.BaseStream.Seek((int)offset, SeekOrigin.Begin);
+                wowWriter.Write(pBytes);
+                wowWriter.Close();
+
+                Console.WriteLine("Done.");
+            }
+        }
+    }
+}
