d471c0c70bce99e4e05c986901a953db47a9ee43
Mansemino <mansemino2@gmail.com>
2012-12-09 01:21:24 +0100

    Updated patch for x64 client & restore option
    Added the address & data to patch the x64 client.
    Also, added original data & option to restore client to its original
    state.
    Added icon to main program & message info if clicked & no file dropped.

--- Client Patcher/Source/Arctium Connection Patcher/Addresses.cs
+++ Client Patcher/Source/Arctium Connection Patcher/Addresses.cs
@@ -1,19 +1,30 @@
 ﻿namespace ArctiumConnectionPatcher
 {
-    enum Offsets : int
+    enum Offsetsx32 : int
     {
         // Client movement packets
         SendOffset               = 0x40B9F9,
         SendOffset2              = 0x40BA09,
         SendOffset3              = 0x40BA16,
-
         // Client packets
         LegacyRoutingTableOffset = 0xA481F0,
-
         // Some server packets
         CommsHandlerOffset       = 0x40B3CF,
-
         // Allow login with email addresses
         emailOffset              = 0x7C40D6
     }
+    enum Offsetsx64 : int
+    {
+        // Client movement packets
+        SendOffset               = 0x4D0AF6,
+        SendOffset2              = 0x4D0B04,
+        SendOffset3              = 0x4D0B11,
+        // Client packets
+        LegacyRoutingTableOffset = 0xC54110,
+        // Some server packets
+        CommsHandlerOffset       = 0x4D0226,
+        // Allow login with email addresses
+        emailOffset              = 0x95C42D
+    }
+
 }
