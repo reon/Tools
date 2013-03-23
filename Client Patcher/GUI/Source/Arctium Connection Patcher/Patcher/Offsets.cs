1c38a5c0de7c01d574c62726f6c9baf840e7ff11
Fabian <fabian@arctium.org>
2013-03-23 00:24:42 -0700

    Added GUI patcher source.

---
+++ Client Patcher/GUI/Source/Arctium Connection Patcher/Patcher/Offsets.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Copyright (C) 2012-2013 Arctium <http://arctium.org>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Arctium_Connection_Patcher.Patcher
+{
+    public class Offset
+    {
+        public enum x86
+        {
+            Movement     = 0x448485,
+            Movement2    = 0x448492,
+            Movement3    = 0x4484A2,
+            Movement4    = 0x4484AF,
+            Legacy       = 0x448480,
+            Email        = 0x801616,
+            User         = 0x2E54C5
+        }
+
+        public enum x64
+        {
+            Movement     = 0,
+            Movement2    = 0,
+            Movement3    = 0,
+            Movement4    = 0,
+            Legacy       = 0,
+            Email        = 0,
+            User         = 0
+        }
+    }
+}
