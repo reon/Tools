f11548b4fc6d2671af4bd57b74fbf64aaf1ffdbe
Fabian <fabian@arctium.org>
2015-04-24 22:53:10 +0200

    Added SkillRaceClassInfo & SpellLevels structures.

---
+++ World of Warcraft/Bins/DataExtractor/Structures/SkillRaceClassInfo.cs
@@ -0,0 +1,16 @@
+using DataExtractor;
+
+namespace DataExtractor.Structures
+{
+    public class SkillRaceClassInfo
+    {
+        public int Id;
+        public int SkillId;
+        public int RaceMask;
+        public int ClassMask;
+        public int Flags;
+        public int Availability;
+        public int MinLevel;
+        public int SkillTierId;
+    }
+}
---
+++ World of Warcraft/Bins/DataExtractor/Structures/SpellLevels.cs
@@ -0,0 +1,14 @@
+using DataExtractor;
+
+namespace DataExtractor.Structures
+{
+    public class SpellLevels
+    {
+        public int Id;
+        public int SpellId;
+        public int DifficultyId;
+        public int BaseLevel;
+        public int MaxLevel;
+        public int SpellLevel;
+    }
+}
