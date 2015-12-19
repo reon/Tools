998185b970678c271ab86f926b13511c77b11fba
Fabian <fabian@arctium-emulation.com>
2015-12-18 19:11:51 -0800

    Updated DataExtractor for Legion build 20810+

---
+++ World of Warcraft/Source/DataExtractor/Constants/FileFlags.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Arctium Emulation.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+namespace DataExtractor.Constants
+{
+    [Flags]
+    enum FileFlags : int
+    {
+        None       = 0,
+        DataOffset = 1,
+        Unknown    = 2, // Some Index data stuff?!
+        Index      = 4,
+    }
+}
